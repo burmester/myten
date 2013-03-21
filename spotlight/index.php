8f3f9c15f984a1ecd536d18ecf3f5cc024d4f57a
Johan Burmester <johanbur@kth.se>
2013-03-21 20:09:16 +0100

    reupload johanburmester.se

---
+++ spotlight/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+<link href="stylesheet.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="container">
+    <div class="header"><div class="overlay"></div></div>
+    <div class="scen"><div class="overlay"></div></div>
+	<div class="lightning">
+		<div class="spotlight" id="farleft">
+            <div class="lightbeam"></div>
+            <div class="crop"><div class="extraomni"></div></div>
+            <div class="omni">
+            <div class="text" style="-webkit-transform: matrix3d(1.5, 0, 0, 0,
+            				    -1, 1, 0, 0.005,
+                                0, 0, 1, 0, 
+                                40, 50, 0, 2);">
+                <div class="shadow" ></div>
+            	<div class="spinit"><img src="rocker2.png" /></div>
+            </div>
+            </div><!--omni-->
+        </div><!--spotlight-->
+		<div class="spotlight" style="left:250px;-webkit-transform: matrix3d(0.8, 0, 0, 0,
+            				    -0.1, 0.7, 0, -0.005,
+                                0, 0, 1, 0, 
+                                0, 0, 0, 2);">
+            <div class="lightbeam"></div>
+            <div class="crop"><div class="extraomni"></div></div>
+            <div class="omni">
+            
+            </div><!--omni-->
+        </div><!--spotlight-->
+        		<div class="spotlight" style="right:250px;-webkit-transform: matrix3d(0.8, 0, 0, 0,
+            				    0.1, 0.7, 0, -0.005,
+                                0, 0, 1, 0, 
+                                0, 0, 0, 2);">
+            <div class="lightbeam"></div>
+            <div class="crop"><div class="extraomni"></div></div>
+            <div class="omni">
+            
+            </div><!--omni-->
+        </div><!--spotlight-->
+		<div class="spotlight" style="right:400px;-webkit-transform: matrix3d(0.8, 0, 0, 0,
+            				    -0.5, 0.7, 0, -0.005,
+                                0, 0, 1, 0, 
+                                0, 0, 0, 2);">
+            <div class="lightbeam"></div>
+            <div class="crop"><div class="extraomni"></div></div>
+            <div class="omni">
+            
+            </div><!--omni-->
+        </div><!--spotlight-->
+	</div><!--lightning-->
+    <div class="content">
+    <div class="contentLeft shadow"><div class="overlay"></div></div>
+    <div class="contentRight shadow"><div class="overlay"></div></div>
+    </div>    
+</div>
+</body>
+</html>
